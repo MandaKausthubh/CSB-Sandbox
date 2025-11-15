f7170af078618da15d5e8c2b590534c28db5248d
Manda Kausthubh <manda.kausthubh@gmail.com>
2025-11-16 01:17:17 +0530

    Force through the conflicts

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,15 @@ from core.auth import user_dependancy
 from database.database import engine, get_db
 import core.auth as auth
 import core.users as users
+from fastapi.middleware.cors import CORSMiddleware
 
+
+#
+# app = FastAPI()
+# app.include_router(auth.router, prefix="/api")
+# app.include_router(users.router, prefix="/api")
+# user_models.Base.metadata.create_all(bind=engine)  # Ensure tables are created
+#
 app = FastAPI()
 app.include_router(auth.router, prefix="/api")
 app.include_router(users.router, prefix="/api")
@@ -20,6 +28,19 @@ analysis_models.Base.metadata.create_all(bind=engine)  # Ensure tables are creat
 def read_api(db: Session = Depends(get_db)):
     return db.query(user_models.UserDB).all()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def root():
+    return {"status": "backend running!"}
 
 @app.get("/user", response_model=user_models.UserResponse)
 def read_user(current_user: user_dependancy, db: Session = Depends(get_db)):
